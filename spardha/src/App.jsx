39329c8095e0047f6c79c8dcc0aec16b316b852e
kunalvy <kunalvyas455@gmail.com>
2024-08-21 01:08:57 +0530

    first commit

---
+++ spardha/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+  <div className='A1'>
+    <a className='a1' href="">About us</a>
+    <a  className ='a2'href="">Events</a>
+     <a className='a3' href="">teams</a>
+     <a className='a4' href="">Sponsers</a>
+
+<div>
+<img id='img2' src="https://www.iitbhu.ac.in/contents/iitbhu/img/other/iit_emblem_original.jpg" alt="" />
+</div>
+<div id='heroimg'>
+
+
+</div >
+<div id='a11'> 
+  <h1 className='h2'> Welcome to Spardha 2024</h1>
+  <h2 className='h2'>18-20 oct 2024</h2>
+</div>
+<footer id='footer'>
+  <div id='footer1'>
+     <div id='footer2'>
+         <ul id='ul'>
+            <li className='li1' ><a  href="">History</a></li>
+            <li className='li1'><a href="">Know our Guests</a></li>
+            <li className='li1'><a href="">Sports and Technology</a></li>
+            <li className='li1'><a href="">Coches and Staff</a></li>
+          </ul>
+      </div>
+  
+    </div>
+</footer>
+
+
+
+
+<div>
+<a  href="https://www.instagram.com/spardha_iitbhu/?hl=en">
+<img id='img1' src="https://img.freepik.com/premium-vector/instagram-vector-logo-icon-social-media-logotype_901408-390.jpg?semt=ais_hybrid" alt="" />
+  </a>
+  <a href="https://www.facebook.com/search/top?q=spardha%20-%20iit%20(bhu)%2C%20varanasi">
+    <img id='img4' src="https://i.pinimg.com/474x/d2/17/4b/d2174bdef984e49aafabeb437744ca7a.jpg" alt="" />
+  </a>
+  <a href="https://x.com/Spardha_IITBHU">
+  <img id='img6' src="https://cdn-icons-png.flaticon.com/512/2496/2496110.png" alt="" />
+  </a>
+  <a href="https://www.linkedin.com/search/results/all/?fetchDeterministicClustersOnly=true&heroEntityKey=urn%3Ali%3Aorganization%3A13326821&keywords=spardha%2C%20iit%20(bhu)%20varanasi&origin=RICH_QUERY_SUGGESTION">
+  <img id='img5' src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-linkedin-512.png" alt="" />
+  </a>
+</div>
+  </div>
+
+
+  )
+}
+
+export default App
